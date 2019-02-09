a4b5111b03408dcf0e0678d193e6ac4085eb2b73
sburns1369 <kiwishoepolish@yahoo.com>
2019-02-08 20:12:19 -0500

    Update test_xgs.sh

--- test_xgs.sh
+++ test_xgs.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-COIN=genesisX
-COINPORT=5555
-COINRPCPORT=19012
-COINDAEMON=genesisd
-COINDAEMONCLI=genesis-cli
-COINDAEMONTX=genesis-tx
-COINCORE=.genesis
-COINCONFIG=genesis.conf
-
+#setting varibles
+COIN=GenesisX
 #Setting Colors
 BLUE='\033[0;96m'
 GREEN='\033[0;92m'
 RED='\033[0;91m'
-YELLOW='\093[0;93m'
+YELLOW='\033[0;93m'
 #clear font
 CLEAR='\033[0m'
 
@@ -22,21 +15,22 @@ if [[ $(lsb_release -d) != *16.04* ]]; then
   exit 1
 fi
 #Menu to Confirm and Pull correct script
-echo -e ${YELLOW}"Welcome to the NullDev Beta Automated Install Script"${CLEAR}
+echo -e ${YELLOW}"Welcome to the $Coin Beta Automated Install Script by NullEntry"${CLEAR}
 echo -e ${YELLOW}'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND'${CLEAR}
 echo -e ${YELLOW}"Durring this Process Please Hit Enter or Input What is Asked."${CLEAR}
 echo
-echo -e ${GREEN}"Are you sure you want to install exdex Masternode? type y/n followed by [ENTER]:"$${CLEAR}
+echo -e ${GREEN}"Are you sure you want to install $COIN Masternode? type y/n followed by [ENTER]:"${CLEAR}
 read AGREE
 if [[ $AGREE =~ "y" ]] ; then
-echo -e ${GREEN}"Please enter the number of $COIN Masternode(s) you would like to install :"${NC}
+echo -e ${GREEN}"Please enter the number (1-4) of $COIN Masternode(s) you would like to install :"${CLEAR}
 read NUMBER
-  if [[$NUMBER = "1"]] ; then
-    echo - "Send Script one!"
-  if [[$NUMBER = "2"]] ; then
-    echo - "Send Script two!"
-  if [[$NUMBER = "3"]] ; then
-    echo - "Send Script three!"
-  if [[$NUMBER = "4"]] ; then
-    echo - "Send Script four!"
+case $NUMBER in
+  1)bash <(curl -Ls https://raw.githubusercontent.com/sburns1369/GenesisX_MN_Script/master/xgs_single.sh);;
+  2)bash <(curl -Ls https://raw.githubusercontent.com/sburns1369/GenesisX_MN_Script/master/xgs_2pack.sh);;
+  3)bash <(curl -Ls https://raw.githubusercontent.com/sburns1369/GenesisX_MN_Script/master/xgs_3pack.sh);;
+  4)bash <(curl -Ls https://raw.githubusercontent.com/sburns1369/GenesisX_MN_Script/master/xgs_4pack.sh);;
+  *)echo -e ${RED}"Invalid Option - Exiting Setup - Re-run to try again"${CLEAR};;
+esac
+else
+echo -e "Exiting Setup!  You can re-run to try again"
 fi
