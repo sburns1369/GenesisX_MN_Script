2c810d1baa8e9599e7a64191aebd5f458ce422dc
sburns1369 <kiwishoepolish@yahoo.com>
2020-11-10 20:46:46 -0500

    missing file for script base

---
+++ foreword.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+BLUE='\033[0;96m'
+CLEAR='\033[0m'
+RED='\033[0;91m'
+echo -e ${BLUE}"   Hi.  If you have used my scripts before, please stand by.  You will no longer need to generate"
+echo -e ${BLUE}"keys.  With an effort to raise the bar from the concept of generic scripts that have simply too many"
+echo -e ${BLUE}"steps are problematic to maintain, and sometimes rough to support for those not familiar with Linux."
+echo -e ${BLUE}"This script was designed for impatient people, people that are not well versed in Linux, and those who"
+echo -e ${BLUE}"are simply curious on how to start up a master node.  I hope this is one of the fastest, most effective,"
+echo -e ${BLUE}"and easiest use Masternode Installation scripts you have and ever will use.  Around 2000 lines of code"
+echo -e ${BLUE}"will be run by this script, and I host nodes for all installations; as well as each script has a bootstrap."
+echo -e ${BLUE}"When complete, you should simply be able to copy and paste (by highlighting) the output.  If for some"
+echo -e ${BLUE}"reason you need to access it again, select option 4 - Display Masternode.conf Information from the main"
+echo -e ${BLUE}"menu.  This script is free to use and distribute, donations are sincerely appreciated to assist in cover"
+echo -e ${BLUE}"the server cost for hosting nodes, boots straps, Red Bull addiction, and so forth.  Not to mention it saves"
+echo -e ${BLUE}"you money from running multiple VPS"
+echo -e ${RED}"                                                                   -SBurns"${CLEAR}
